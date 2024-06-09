2a72d0d816545639c4742916cf76ca83d7ddafec
Nirmal Neupane <neupanenirmal417@gmail.com>
2024-06-09 18:40:38 +0545

    feat: change password ui and integration

---
+++ apps/frontend/src/app/admin/dashboard/change-password/page.tsx
@@ -0,0 +1,8 @@
+import ChangePassword from "@/app/user/change-password/page";
+import React from "react";
+
+const ChangeAdmniPassword = () => {
+  return <ChangePassword />;
+};
+
+export default ChangeAdmniPassword;
